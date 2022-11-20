6ec6f862bed2c783bfc47b20a95279afc6486a6a
Synonymous64 <prajwalurkude007@gmail.com>
2022-11-20 13:10:06 +0530

    Styling and Images Info

--- gecode/pages/index.js
+++ gecode/pages/index.js
@@ -13,10 +13,17 @@ export default function Home() {
         <meta name="keyword" content="nextjs, Geek Coding blogs" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <nav className={styles.mainNav}>
+        <ul>
+          <li>Home</li>
+          <li>about</li>
+          <li>Blog</li>
+          <li>Contact</li>
+        </ul>
+      </nav>
       {/* To load a third-party script in a single route, import next/script and include the script directly in your page component: */}
       {/* The script will only be fetched and executed when this specific page is loaded on the browser. */}
       {/* lazy load */}
-      <Script src='/sc.js'></Script>
       <main className={styles.main}>
         <h1 className={styles.title}>
           GeCode
@@ -27,36 +34,26 @@ export default function Home() {
           there time management by providing optimal solutions for plethoric bugs
         </p>
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
+        <div className="blogs">
+          <div className="blogItem">
+            <h2>Popular Blogs</h2>
+            <h3>How to learn javascript in 2022</h3>
             <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
+              Javascript is a language of the internet. Every browser in the world are fused with Javascript
             </p>
-          </a>
+          </div>
+          <div className="blogItem">
+            <h3>How to learn javascript in 2022</h3>
+            <p>
+              Javascript is a language of the internet. Every browser in the world are fused with Javascript
+            </p>
+          </div>
+          <div className="blogItem">
+            <h3>How to learn javascript in 2022</h3>
+            <p>
+              Javascript is a language of the internet. Every browser in the world are fused with Javascript
+            </p>
+          </div>
         </div>
       </main>
 
