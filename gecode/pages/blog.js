621d5933e6797dd9574e6668a696e7e9acc0308c
Synonymous64 <prajwalurkude007@gmail.com>
2022-12-05 23:51:25 +0530

    Blog Component

--- gecode/pages/blog.js
+++ gecode/pages/blog.js
@@ -1,9 +1,32 @@
 import React from 'react'
-
+import styles from '../styles/Blog.module.css'
+import Link from 'next/link'
 const Blog = () => {
     return (
-        <div>
-            This is my Blog page
+        <div className={`${styles.container}`}>
+            <main className={styles.main}>
+                <div className={styles.blogItem}>
+                    <Link href={'/blogspot/learn-javascript'}>
+                        <h3>How to learn javascript in 2022</h3></Link>
+                    <p>
+                        Javascript is a language of the internet. Every browser in the world are fused with Javascript
+                    </p>
+                </div>
+                <div className={styles.blogItem}>
+                    <Link href={'/blogspot/learn-javascript'}>
+                        <h3>How to learn javascript in 2022</h3></Link>
+                    <p>
+                        Javascript is a language of the internet. Every browser in the world are fused with Javascript
+                    </p>
+                </div>
+                <div className={styles.blogItem}>
+                    <Link href={'/blogspot/learn-javascript'}>
+                        <h3>How to learn javascript in 2022</h3></Link>
+                    <p>
+                        Javascript is a language of the internet. Every browser in the world are fused with Javascript
+                    </p>
+                </div>
+            </main>
         </div>
     )
 }
--- gecode/pages/blogspot/[slug].js
+++ gecode/pages/blogspot/[slug].js
@@ -1,11 +1,18 @@
 import React from 'react'
 import { useRouter } from 'next/router'
+import styles from '../../styles/BlogSpot.module.css'
 const slug = () => {
     const router = useRouter();
-    const {slug} = router.query;
+    const { slug } = router.query;
     return (
-        <div>
-        {slug}
+        <div className={`${styles.container}`}>
+            <main className={styles.main}>
+                <h1>Title of the page {slug}</h1>
+                <hr />
+                <p>
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Provident fugiat labore nemo expedita ad iste distinctio deserunt fuga consequuntur sapiente! Eius autem molestiae mollitia modi excepturi quos quas error nesciunt?
+                </p>
+            </main>
         </div>
     )
 }
