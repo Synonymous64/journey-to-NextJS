0a9b075f482e361bd1e93856a9725e8d097be189
Synonymous64 <prajwalurkude007@gmail.com>
2022-12-10 19:16:15 +0530

    Fetched and Parsed blogs

--- gecode/pages/api/blogs.js
+++ gecode/pages/api/blogs.js
@@ -1,9 +1,29 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import * as fs from 'fs';
-export default function handler(req, res) {
+export default async function handler(req, res) {
   // console.log(res); // it doesn't show up in web console only displayed on the command lines
-  fs.readdir('blogData', (err, data) => {
-    console.log(data);
-    res.status(200).json(data);
-  })
+  // let allBlogs = [];
+  // fs.readdir('blogData', (err, data) => {
+  //   console.log(data);
+  //   data.forEach((item)=>{
+  //     console.log(item);
+  //     fs.readFile('blogData/' + item, 'utf-8', (err, res)=>{
+  //       // console.log(res);
+  //       allBlogs.push(res);
+  //       if(err) console.log('not found');
+  //       console.log(allBlogs);
+  //     })
+  //   })
+  //   res.status(200).json(allBlogs);
+  // })
+  let data = await fs.promises.readdir('blogData');
+  let myFile;
+  let allBlogs = [];
+  for(let item = 0; item < data.length; ++item){
+    console.log(data[item]);
+    myFile = await fs.promises.readFile('blogData/' + data[item], 'utf-8');
+    console.log(myFile);
+    allBlogs.push(JSON.parse(myFile));
+  }
+  res.status(200).json(allBlogs)
 }
--- gecode/pages/blog.js
+++ gecode/pages/blog.js
@@ -1,35 +1,36 @@
 import React from 'react'
 import styles from '../styles/Blog.module.css'
 import Link from 'next/link'
-
+import { useEffect } from 'react'
+import { useState } from 'react'
 // * Step 1: Collect all the files from blog data directory.
 // * Step 2: Iterate through them and display them.
 
 const Blog = () => {
+    const [blogs, setBlogs] = useState([]); 
+    useEffect(()=>{
+        console.log('use effect is running');
+        fetch('http://localhost:3000/api/blogs').then((data)=>{
+            return data.json();
+        }).then((parsedData)=>{
+            console.log(parsedData);
+            setBlogs(parsedData)
+        })
+    }, [])
     return (
         <div className={`${styles.container}`}>
             <main className={styles.main}>
-                <div className={styles.blogItem}>
-                    <Link href={'/blogspot/learn-javascript'}>
-                        <h3>How to learn javascript in 2022</h3></Link>
-                    <p>
-                        Javascript is a language of the internet. Every browser in the world are fused with Javascript
-                    </p>
-                </div>
-                <div className={styles.blogItem}>
-                    <Link href={'/blogspot/learn-javascript'}>
-                        <h3>How to learn javascript in 2022</h3></Link>
-                    <p>
-                        Javascript is a language of the internet. Every browser in the world are fused with Javascript
-                    </p>
-                </div>
-                <div className={styles.blogItem}>
-                    <Link href={'/blogspot/learn-javascript'}>
-                        <h3>How to learn javascript in 2022</h3></Link>
-                    <p>
-                        Javascript is a language of the internet. Every browser in the world are fused with Javascript
-                    </p>
-                </div>
+                {blogs.map((blogItem)=>{
+                    return (
+                        <div className={styles.blogItem} key={blogItem.slug}>
+                            <Link href={`/blogspot/${blogItem.slug}`}>
+                                <h3>{blogItem.title}</h3></Link>
+                            <p className={styles.blogItemP}>
+                                {blogItem.content.substr(0, 140)+"..."}
+                            </p>
+                        </div>
+                    )
+                })}
             </main>
         </div>
     )
