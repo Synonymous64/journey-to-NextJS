613726ccc627a13805485950c4ee18b862a93f0f
Synonymous64 <prajwalurkude007@gmail.com>
2022-12-20 21:46:25 +0530

    Server site rendering with the help of serversiteprops

--- gecode/pages/blog.js
+++ gecode/pages/blog.js
@@ -6,27 +6,28 @@ import { useState } from 'react'
 // * Step 1: Collect all the files from blog data directory.
 // * Step 2: Iterate through them and display them.
 
-const Blog = () => {
-    const [blogs, setBlogs] = useState([]); 
-    useEffect(()=>{
-        // console.log('use effect is running');
-        fetch('http://localhost:3000/api/blogs').then((data)=>{
-            return data.json();
-        }).then((parsedData)=>{
-            // console.log(parsedData);
-            setBlogs(parsedData)
-        })
-    }, [])
+const Blog = ({ myBlogs }) => {
+    const [blogs, setBlogs] = useState(myBlogs);
+    console.log(myBlogs);
+    // useEffect(()=>{
+    //     // console.log('use effect is running');
+    //     fetch('http://localhost:3000/api/blogs').then((data)=>{
+    //         return data.json();
+    //     }).then((parsedData)=>{
+    //         // console.log(parsedData);
+    //         setBlogs(parsedData)
+    //     })
+    // }, [])
     return (
         <div className={`${styles.container}`}>
             <main className={styles.main}>
-                {blogs.map((blogItem)=>{
+                {blogs.map((blogItem) => {
                     return (
                         <div className={styles.blogItem} key={blogItem.slug}>
                             <Link href={`/blogspot/${blogItem.slug}`}>
                                 <h3>{blogItem.title}</h3></Link>
                             <p className={styles.blogItemP}>
-                                {blogItem.content.substr(0, 140)+"..."}
+                                {blogItem.content.substr(0, 140) + "..."}
                             </p>
                         </div>
                     )
@@ -34,6 +35,13 @@ const Blog = () => {
             </main>
         </div>
     )
+}
+export async function getServerSideProps(context) {
+    let data = await fetch('http://localhost:3000/api/blogs');
+    let myBlogs = await data.json();
+    return {
+        props: { myBlogs }, // will be passed to the page component as props
+    }
 }
 
 export default Blog
--- gecode/pages/blogspot/[slug].js
+++ gecode/pages/blogspot/[slug].js
@@ -3,7 +3,7 @@ import { useRouter } from 'next/router'
 import styles from '../../styles/BlogSpot.module.css'
 // * Step 1: Find the file corresponding to the file.
 // * Step 2: Populate them inside the page.
-const slug = () => {
+const slug = ({ blogData }) => {
     // let api = "https://anapioficeandfire.com/api/characters/583";
     // const fetchContent = async (api) => {
     //     let p = await fetch(api);
@@ -20,30 +20,39 @@ const slug = () => {
     // }
     // let p = fetchApi();
     // console.log(p);
-    const [blog, setBlog] = useState();
-    const router = useRouter();
-    const { slug } = router.query;
-    useEffect(()=>{
-        // console.log('use effect is running');
-        if(!router.isReady) return;
-        fetch(`http://localhost:3000/api/getBlog?slug=${slug}`).then((data)=>{
-            return data.json();
-        }).then((parsedData)=>{
-            // console.log(parsedData);
-            setBlog(parsedData)
-        })
-    }, [router.isReady])
+    const [blog, setBlog] = useState(blogData);
+    // const router = useRouter();
+    // const { slug } = router.query;
+    // useEffect(() => {
+    //     // console.log('use effect is running');
+    //     if (!router.isReady) return;
+    //     fetch(`http://localhost:3000/api/getBlog?slug=${slug}`).then((data) => {
+    //         return data.json();
+    //     }).then((parsedData) => {
+    //         // console.log(parsedData);
+    //         setBlog(parsedData)
+    //     })
+    // }, [router.isReady])
     return (
         <div className={`${styles.container}`}>
             <main className={styles.main}>
-                <h1>{ blog && blog.title}</h1>
+                <h1>{blog && blog.title}</h1>
                 <hr />
                 <p>
-                    { blog && blog.content}
+                    {blog && blog.content}
                 </p>
             </main>
         </div>
     )
 }
-
+export async function getServerSideProps(context) {
+    // console.log('use effect is running');
+    // console.log(context.req.url);
+    const { slug } = context.query;
+    let data = await fetch(`http://localhost:3000/api/getBlog?slug=${slug}`)
+    let blogData = await data.json();
+    return {
+        props: { blogData }, // will be passed to the page component as props
+    };
+}
 export default slug
